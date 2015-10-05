57ed3774bd371a5161f93029719d37c5abc36c34
Duncan Regan <duncanregan@gmail.com>
2015-10-05 12:51:14 -0400

    adds a string to buffer method

--- src/buffers.js
+++ src/buffers.js
@@ -1,9 +1,21 @@
-var stringToBuffer = function(string){
-  return bufffer;
+var stringToBuffer = function(str){
+  if (typeof str !== 'string') {
+    throw TypeError('Argument needs to be a string');
+    return;
+  }
+
+  // let arrayBuffer = new ArrayBuffer(str.length * 2);
+  let convertedString = [].map.call(str, (char, i) => {
+    return str.charCodeAt(i);
+  });
+
+  let buffer = new Uint16Array(convertedString);
+
+  return buffer;
 };
 
 var bufferToString = function(buffer){
-  return string;
+  return str;
 };
 
 export default { stringToBuffer, bufferToString };
